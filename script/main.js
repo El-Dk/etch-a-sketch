8ea50a6b4b1fc98e18013b4f3649f1fe2f606323
Derek <briannish@gmail.com>
2021-08-02 22:18:42 -0400

    Add paintSquare function with opacity

--- script/main.js
+++ script/main.js
@@ -1,5 +1,6 @@
 const completeGrid = document.querySelector('#completeGrid');
-const GRIDSIZE = 25;
+const GRIDSIZE = 28;
+let colorChosen = 100;
 
 completeGrid.style.backgroundColor = "red";
 
@@ -12,8 +13,18 @@ function createGrid(size){
             square.classList.add('square');
             square.style.width = `${GRIDSIZE / size}rem`;
             square.style.height = `${GRIDSIZE / size}rem`;
+            square.setAttribute('data-opacity', 0);
+            square.addEventListener('mouseover', paintSquare);
             completeGrid.appendChild(square);
         }
+    }
+}
+function paintSquare(elem){
+    let squareOpacity = Number(elem.target.getAttribute('data-opacity'));
+    if(squareOpacity < 1){
+        squareOpacity +=0.1;
+        elem.target.style.backgroundColor = `hsla(${colorChosen}, 100%, 50%, ${squareOpacity})`;
+        elem.target.setAttribute('data-opacity', squareOpacity);
     }
 }
 createGrid(Number(prompt("Size:")));
